eb8033eb97c0c50ec9b0ff56b76417fa3ffa1ddb
rommelbin <lebedev2002danil@mail.ru>
2020-12-03 01:14:30 +0700

    Lesson 2

--- src/components/index/catalog.js
+++ src/components/index/catalog.js
@@ -35,7 +35,7 @@ function initCatalog() {
                     // console.log('КУПЛЕНО!')
                     let id = event.target.dataset.id; //from data-id
                     let item = this.items.find(el => el.productId == id);
-                    this.basket.add(item);
+                    basket.add(item)
                 }
             });
         }
@@ -68,3 +68,72 @@ function renderCatalogTemplate(item) {
         </div>
     `
 }
+class Catalog {
+    items = []
+    container = null
+    basket = null
+    url = 'https://raw.githubusercontent.com/kellolo/static/master/JSON/catalog.json'
+
+    constructor(basket) {
+        this.container = document.querySelector('#catalog');
+        this.basket = basket;
+
+        //async
+        this._get(this.url)
+        .then(catalog => {
+            this.items = catalog;
+            this._render();
+            this._handleEvents();
+        });
+    }
+    
+    async _get(url) {
+        const d = await fetch(url);
+        return await d.json(); //сделает запрос за джейсоном, дождется ответа и преобразует джейсон в объект, который вернется из данного метода
+    }
+
+    _render() {
+        let htmlStr = '';
+
+        this.items.forEach((item, i) => {
+            htmlStr += this.renderCatalogTemplate(item, i);
+        });
+        this.container.innerHTML = htmlStr;
+    }
+
+    _handleEvents() {
+        this.container.addEventListener('click', event => {
+            if(event.target.name == 'add') {
+                // console.log('КУПЛЕНО!')
+                let id = event.target.dataset.id; //from data-id
+                let item = this.items.find(el => el.productId == id);
+                basket.add(item)
+            }
+        });
+    }
+    renderCatalogTemplate(item) {
+        return `
+            <div class="featuredItem">
+                <div class="featuredImgWrap">
+                    <div class="featuredBuy">
+                        <button 
+                            name="add"
+                            data-id="${item.productId}"
+                        >
+                            <img src="../src/assets/images/addToCart.png" alt="">
+                            Add to Cart
+                        </button>
+                    </div>
+                    <img class="featuredProduct" src="${item.productImg}" alt="">
+                </div>
+                <div class="featuredNameAndPrice">
+                    <div class="featuredItemName">
+                        ${item.productName}
+                    </div>
+                    <div class="featuredItemPrice">$${item.productPrice}</div>
+                </div>
+            </div>
+        `
+    }
+}
+const catalog = new Catalog(basket)
